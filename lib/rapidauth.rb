e57d9e34425bb2892b5172d0427d1f3e158c23e7
nicobertin <bertin@live.cl>
2023-04-04 11:25:18 -0400

    init

---
+++ lib/rapidauth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rapidauth/version"
+
+module Rapidauth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rapidauth/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rapidauth
+  VERSION = "0.1.0"
+end
